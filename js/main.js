962e2af417a3c4260e23b54f872619bb62eb5de7
Дмитрий Антонов <nagi@iMac-Dmitrij-2.local>
2019-06-19 16:48:58 +0300

    новости список + деталка

--- js/main.js
+++ js/main.js
@@ -3,65 +3,96 @@ $(document).ready(function(){
   sliders();
 })
 custom = function(){
-  $('.cases .item').each(function(){
-    var text_temp = $(this).find('p.two').text(),
-    temp = ($(this).index()+1)%12
-    console.log($(this).index())
-    if(temp == (2 || 8 || 10)){
-      sliced = text_temp.slice(0,170)
-    }else{
-      sliced = text_temp.slice(0,80)
-    }
+  short_text = function(){
+    $('.cases .item').each(function(){
+      var text_temp = $(this).find('p.two').text(),
+      temp = ($(this).index()+1)%12
+      console.log($(this).index())
+      if(temp == 2 ||temp ==  8 || temp == 10){
+        sliced = text_temp.slice(0,170)
+      }else{
+        sliced = text_temp.slice(0,80)
+      }
+  
+      if (sliced.length < text_temp.length) {
+        sliced += '...';
+      }
+      $(this).find('p.two').text(sliced)
+    })
 
-    if (sliced.length < text_temp.length) {
-      sliced += '...';
-    }
-    $(this).find('p.two').text(sliced)
-  })
-  $('.news .item').each(function(){
-    var text_temp = $(this).find('p.two').text(),
-    temp = ($(this).index()+1)%12
-    console.log($(this).index())
-    if(temp == (1 || 7 || 9)){
-      sliced = text_temp.slice(0,200)
-    }else{
+
+    $('.list_news_sub .item').each(function(){
+      var text_temp = $(this).find('p.two').text(),
+      temp = ($(this).index()+1)%12
+      console.log($(this).index())
+      if(temp == 1 ||temp ==  7 ||temp ==  9){
+        sliced = text_temp.slice(0,200)
+      }else{
+        sliced = text_temp.slice(0,110)
+      }
+  
+      if (sliced.length < text_temp.length) {
+        sliced += '...';
+      }
+      $(this).find('p.two').text(sliced)
+    })
+    $('.read_yet .list .item').each(function(){
+      var text_temp = $(this).find('p.two').text();
       sliced = text_temp.slice(0,110)
-    }
-
-    if (sliced.length < text_temp.length) {
-      sliced += '...';
-    }
-    $(this).find('p.two').text(sliced)
-  })
-
-  $('.dropdown-wrapper .block').click(function(){
-    if($(this).parent().hasClass('active')){
+      if (sliced.length < text_temp.length) {
+        sliced += '...';
+      }
+      $(this).find('p.two').text(sliced)
+    })
+    $('.list_news .list .item').each(function(){
+      var text_temp = $(this).find('p.two').text(),
+      temp = ($(this).index()+1)%12
+      console.log($(this).index())
+      if(temp == 1 ||temp ==  7 ||temp ==  9){
+        sliced = text_temp.slice(0,200)
+      }else{
+        sliced = text_temp.slice(0,110)
+      }
+  
+      if (sliced.length < text_temp.length) {
+        sliced += '...';
+      }
+      $(this).find('p.two').text(sliced)
+    })
+  }
+  drop_down_init = function(){
+    $('.dropdown-wrapper .block').click(function(){
+      if($(this).parent().hasClass('active')){
+        $('.dropdown-wrapper ').removeClass('active')
+        $(this).parent().find('.list').height(31)
+        setTimeout(function(){
+          $('.dropdown-wrapper .list').addClass('hide')
+        },300)
+      }else{
+        var Height_temp = 0;
+        $(this).parent().find('.list li a').each(function(){
+          temp = $(this).height()
+          if(!$(this).parent().hasClass('hide'))
+            Height_temp += temp + 12;
+        })
+        $(this).parent().addClass('active')
+        $(this).parent().find('.list').height(Height_temp).removeClass('hide')
+      }
+    })
+    $('.dropdown-wrapper .list li').click(function(){
       $('.dropdown-wrapper ').removeClass('active')
-      $(this).parent().find('.list').height(31)
-      setTimeout(function(){
-        $('.dropdown-wrapper .list').addClass('hide')
-      },300)
-    }else{
-      var Height_temp = 0;
-      $(this).parent().find('.list li a').each(function(){
-        temp = $(this).height()
-        if(!$(this).parent().hasClass('hide'))
-          Height_temp += temp + 12;
-      })
-      $(this).parent().addClass('active')
-      $(this).parent().find('.list').height(Height_temp).removeClass('hide')
-    }
-  })
-  $('.dropdown-wrapper .list li').click(function(){
-    $('.dropdown-wrapper ').removeClass('active')
-    $(this).parent().height(43)
-      setTimeout(function(){
-        $('.dropdown-wrapper .list').addClass('hide')
-      },300)
-    $('.dropdown-wrapper .list li').removeClass('hide')
-    $(this).parent().parent().find('.block .selected').html($(this).html());
-    $(this).addClass('hide')
-  })
+      $(this).parent().height(43)
+        setTimeout(function(){
+          $('.dropdown-wrapper .list').addClass('hide')
+        },300)
+      $('.dropdown-wrapper .list li').removeClass('hide')
+      $(this).parent().parent().find('.block .selected').html($(this).html());
+      $(this).addClass('hide')
+    })
+  }
+  
+  drop_down_init();
+  short_text();
 };
 sliders = function(){
   sliders_init = function(){
